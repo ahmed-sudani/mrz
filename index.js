7d01fff72460b5383e2a6481da0884f1a814083f
ahmed-sudani <47687479+ahmed-sudani@users.noreply.github.com>
2021-01-17 20:48:32 +0200

    the global.rootPath
    the global.rootPath to specify a main path or root folder to use it in different places in the project

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 require("dotenv").config();
-const express = require('express');
-const mrzRoute = require('./routes/mrz.recognize.route');
+const express = require("express");
+const mrzRoute = require("./routes/mrz.recognize.route");
 const helmet = require("helmet");
-const cors = require('cors');
+const cors = require("cors");
+global.rootPath = __dirname;
 
-let app = express()
+let app = express();
 
 //allow cors rigin
-app.use(cors())
+app.use(cors());
 //securite issu
-app.use(helmet())
+app.use(helmet());
 //use routes
 
-app.get("/", (req, res)=>{res.send("OK")})
+app.get("/", (req, res) => {
+  res.send("OK");
+});
 
-app.use(mrzRoute)
- 
+app.use(mrzRoute);
+
 //transfare 404 error to error handler
-app.use((req, res, next)=>{
-  next({code : 404})
-})
+app.use((req, res, next) => {
+  next({ code: 404 });
+});
 
 //error handler
-app.use((err, req, res, next)=>{
-  if(err.code){
-    return res.status(err.code).json({err : err})
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.code) {
+    return res.status(err.code).json({ err: err });
   }
-  res.status(400)
+  res.status(400);
   if (req.fileValidationError) {
     return res.send(req.fileValidationError);
   }
   if (!req.file) {
-    return res.send('Please select an image to upload');
+    return res.send("Please select an image to upload");
   }
   if (err) {
     return res.send(err);
   }
-})
+});
 
 //app listen
-app.listen(process.env.PORT || 8001, (err)=>{
+app.listen(process.env.PORT || 8001, (err) => {
   console.log(`server listening on port ${process.env.PORT || 8001}`);
-})
+});
