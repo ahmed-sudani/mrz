9e11fbf5cb0cefb843bc73d152879abdbff1198f
ahmed-sudani <47687479+ahmed-sudani@users.noreply.github.com>
2021-01-17 20:34:13 +0200

    moving from py to js

--- midelwares/dataExtraction.js
+++ midelwares/dataExtraction.js
@@ -1,19 +1,38 @@
-const mrzService = require('../services/mrz.service');
-const {exec} = require("child_process");
+const mrzService = require("../services/mrz.service");
+// const { exec } = require("child_process");
+const jimp = require("jimp");
+const Tesseract = require("node-tesseract-ocr");
 
-let dataExtraction = (req, res, next)=>{
-    exec("python ./services/imageToString.service.py", (error, stdout, stderr)=>{
-        if(error){
-            return next({code : 400, message : "plase enter a valid documnet"})
+let dataExtraction = (req, res, next) => {
+  jimp
+    .read(global.rootPath + `/temp/${req.file.originalname + "passport.jpg"}`)
+    .then(function (lenna) {
+      lenna
+        .quality(100) // set JPEG quality
+        .greyscale()
+        // .contrast(1)
+        .write(
+          global.rootPath + `/temp/${req.file.originalname + "passport.jpg"}`
+        ); // set greyscale
+      return Tesseract.recognize(
+        global.rootPath + `/temp/${req.file.originalname + "passport.jpg"}`,
+        {
+          lang: "ocrb",
+          oem: 1,
+          psm: 3,
         }
-        if(stdout){
-          stdout = mrzService.filterString(stdout)
-          req.mrzLines = stdout
-          mrzService.clearTemp()
-          return next()
-        }
-        return next({code : 400, message : "plase enter a valid documnet"})
+      );
     })
-}
+    .then((data) => {
+      req.mrzLines = mrzService.filterString(data);
+      mrzService.clearTemp(
+        global.rootPath + `/temp/${req.file.originalname + "passport.jpg"}`
+      );
+      next();
+    })
+    .catch(function (err) {
+      next({ err, code: 400 });
+    });
+};
 
-module.exports = dataExtraction
+module.exports = dataExtraction;
